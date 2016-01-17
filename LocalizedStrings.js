8229b27d6bf9bb8d268ab51b29ae7e18867a8f1d
Stefano Falda <stefano.falda@gmail.com>
2016-01-17 23:03:56 +0100

    First commit

---
+++ LocalizedStrings.js
@@ -0,0 +1,102 @@
+'use strict';
+/**
+ * Simple module to localize the React interface using the same syntax 
+ * used in the ReactNativeLocalization module 
+ * (https://github.com/stefalda/ReactNativeLocalization)
+ *
+ * Originally developed by Stefano Falda (stefano.falda@gmail.com)
+ *
+ * It uses a call to the Navigator/Browser object to get the current interface language,
+ * then display the correct language strings or the default language (the first
+ * one if a match is not found).
+ *
+ * How to use:
+ * Check the instructions at:
+ * https://github.com/stefalda/react-localization
+ */
+export default class LocalizedStrings{
+
+  _getBestMatchingLanguage(language, props){
+    //If an object with the passed language key exists return it
+    if (props[language]) return language;
+    //if the string is composed try to find a match with only the first language identifiers (en-US --> en)
+    let idx = language.indexOf("-");
+    if (idx>=0) {
+      language = language.substring(0,idx);
+      if (props[language]) return language;
+    }
+    //Return the default language (the first coded)
+    return Object.keys(props)[0];
+  }
+
+
+  constructor(props) {
+    let interfaceLanguage = (navigator.language || navigator.userLanguage);
+    //Store locally the passed strings
+    this.props = props;
+    //Set language to its default value (the interface)
+    this.setLanguage(interfaceLanguage);
+  }
+
+  //Can be used from ouside the class to force a particular language
+  //indipendently from the interface one
+  setLanguage(language){
+        //Check if exists a translation for the current language or if the default
+        //should be used
+        var bestLanguage = this._getBestMatchingLanguage(language, this.props);
+        this.language = bestLanguage;
+        //Associate the language object to the this object
+        if (this.props[bestLanguage]){
+          //console.log("There are strings for the language:"+language);
+          var localizedStrings = this.props[this.language];
+          for (var key in localizedStrings){
+            //console.log("Checking property:"+key);
+            if (localizedStrings.hasOwnProperty(key)) {
+              //console.log("Associating property:"+key);
+              this[key] = localizedStrings[key];
+            }
+          }
+        }
+    }
+
+  //The current language displayed (could differ from the interface language
+  // if it has been forced manually and a matching translation has been found)
+  getLanguage(){
+    return this.language;
+  }
+
+  //The current interface language (could differ from the language displayed)
+  getInterfaceLanguage(){
+    return interfaceLanguage;
+  }
+
+  //Return an array containing the available languages passed as props in the constructor
+  getAvailableLanguages(){
+    if (!this.availableLanguages){
+      this.availableLanguages = [];
+      for(let language in this.props){
+         this.availableLanguages.push(language);
+      }
+    }
+    return this.availableLanguages;
+  }
+
+  //Format the passed string replacing the numbered placeholders
+  //i.e. I'd like some {0} and {1}, or just {0}
+  //Use example:
+  //  strings.formatString(strings.question, strings.bread, strings.butter)
+  formatString(str, ...values){
+      var res = str;
+      for (let i=0; i<values.length;i++){
+          res = this._replaceAll("{"+i+"}", values[i], res);
+      }
+      return res;
+  }
+
+  //Replace all occorrencies of a string in another using RegExp
+  _replaceAll(find, replace, str) {
+    //Escape find
+    find = find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+    return str.replace(new RegExp(find, 'g'), replace);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # react-localization
-Simple module to localize the React interface using the same syntax used in the ReactNativeLocalization module.
+Simple module to localize the React interface using the same syntax used in the 
+[ReactNativeLocalization module](https://github.com/stefalda/ReactNativeLocalization/).
+
+## How it works
+
+The library uses the current interface language, then it loads and displays the strings matching the current interface locale or the default language (the first one if a match is not found) if a specific localization can't be found.
+
+It's possible to force a language different from the interface one.
+
+## Usage
+
+In the React class that you want to localize require the library and define the strings object passing to the constructor a simple object containing a language key (i.e. en, it, fr..) and then a list of key-value pairs with the needed localized strings.
+
+ ```js
+\\ES6 module syntax
+import LocalizedStrings from 'react-localization';
+
+let strings = new LocalizedStrings({
+  en:{
+    how:"How do you want your egg today?",
+    boiledEgg:"Boiled egg",
+    softBoiledEgg:"Soft-boiled egg",
+    choice:"How to choose the egg"
+  },
+  it: {
+    how:"Come vuoi il tuo uovo oggi?",
+    boiledEgg:"Uovo sodo",
+    softBoiledEgg:"Uovo alla coque",
+    choice:"Come scegliere l'uovo"
+  }
+});
+```
+
+Then use the `strings` object literal directly in the render method accessing the key of the localized string.
+
+```js
+<Text style={styles.title}>
+  {strings.how}
+</Text>
+```
+
+## API
+
+* setLanguage(languageCode) - to force manually a particular language
+* getLanguage() - to get the current displayed language
+* getInterfaceLanguage() - to get the current device interface language
+* formatString() - to format the passed string replacing its placeholders with the other arguments strings
+
+```js
+  en:{
+    bread:"bread",
+    butter:"butter",
+    question:"I'd like {0} and {1}, or just {0}"
+  }
+  ...
+  strings.formatString(strings.question, strings.bread, strings.butter)
+```
+**Beware: do not define a string key as formatString!**
+* getAvailableLanguages() - to get an array of the languages passed in the constructor
+
+## Examples
+To force a particular language use something like this:
+
+```js
+_onSetLanguageToItalian() {
+  strings.setLanguage('it');
+  this.setState({});
+}
+```
+
+## Questions or suggestions?
+Feel free to contact me on [Twitter](https://twitter.com/talpaz) or [open an issue](https://github.com/stefalda/react-localization/issues/new).
+
