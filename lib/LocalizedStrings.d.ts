c7ecff77b7aa1e33545d99c6e7019f41fe9daeaa
her0e1c1 <her0e1c1@gmail.com>
2017-04-20 21:19:43 +0800

    Enable to import interfaces from d.ts

--- lib/LocalizedStrings.d.ts
+++ lib/LocalizedStrings.d.ts
@@ -1,17 +1,18 @@
+
+interface LocalizedStringsMethods {
+    setLanguage(language: string): void;
+    getLanguage(): string;
+    getInterfaceLanguage(): string;
+    formatString(str: string, ...values: any[]): string;
+    getAvailableLanguages(): string[];
+    getString(key:string, language:string):string;
+}
+
+interface LocalizedStringsFactory {
+    new (props: any): LocalizedStringsMethods;
+}
+
 declare module "react-localization" {
-    interface LocalizedStringsMethods {
-        setLanguage(language: string): void;
-        getLanguage(): string;
-        getInterfaceLanguage(): string;
-        formatString(str: string, ...values: any[]): string;
-        getAvailableLanguages(): string[];
-        getString(key:string, language:string):string;
-    }
-
-    interface LocalizedStringsFactory {
-        new (props: any): LocalizedStringsMethods;
-    }
-
     var LocalizedStrings: LocalizedStringsFactory;
     export = LocalizedStrings;
 }
