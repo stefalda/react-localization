a26b8b7f33a43a8080cf2e4b326c233240ea9ebf
Stanley Goldman <StanleyGoldman@github.com>
2018-01-26 14:56:08 -0500

    Exposing LocalizedStrings<T>

--- lib/LocalizedStrings.d.ts
+++ lib/LocalizedStrings.d.ts
@@ -2,12 +2,12 @@ declare module 'react-localization' {
     type Formatted = number | string | JSX.Element;
     type FormatObject<U extends Formatted> = { [key: string]: U };
     
-    export interface GlobalStrings<T> {		
-      [language: string]: T;		
+    export interface GlobalStrings<T> {        
+      [language: string]: T;        
     }
     
   
-    interface LocalizedStringsMethods {
+    export interface LocalizedStringsMethods {
         setLanguage(language: string): void;
         getLanguage(): string;
         getInterfaceLanguage(): string;
@@ -16,11 +16,13 @@ declare module 'react-localization' {
         getString(key: string, language: string): string;
         setContent(props: any): void;
     }
+
+    export type LocalizedStrings<T> = LocalizedStringsMethods & T;
   
     interface LocalizedStringsFactory {
-        new <T>(props: GlobalStrings<T>): LocalizedStringsMethods & T;
+        new <T>(props: GlobalStrings<T>): LocalizedStrings<T>;
     }
   
-    var LocalizedStrings: LocalizedStringsFactory;
-    export default LocalizedStrings;
+    var LocalizedStringsFactory: LocalizedStringsFactory;
+    export default LocalizedStringsFactory;
   }
